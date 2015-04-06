1138fe6f34cac1c54677347e9155c5e08db5eb58
alatsombath <alatsombath@users.noreply.github.com>
2015-04-05 21:00:40 -0700

    1.1 (19 March 2015)
    - Adjusted rotation sensitivity from a linear to exponential scale for
    sound levels, increasing overall fluidity
    - Also adjusted rotation sensitivity and angle change intensity to an
    exponential scale based on frequency, meaning the bass attack is more
    prominent through responsive angle changes and the treble sustain is
    more prominent through responsive rotation speed (The inverse appears to
    be inaccurate for this type of visualizer.)
    - Removed option for backwards rotation on regressing sound levels
    - Decreased default maximum fill angle (AngleFillMax (Rad(360)) ->
    (Rad(180))) and increased default length and gap (RoundlineLength 6 ->
    8, RoundlineGap 2 -> 4)

--- Skins/Eye of the Storm/@Resources/EyeoftheStorm.lua
+++ Skins/Eye of the Storm/@Resources/EyeoftheStorm.lua
@@ -1,10 +1,10 @@
--- EyeoftheStorm v1.0
+-- EyeoftheStorm v1.1
 -- LICENSE: Creative Commons Attribution-Non-Commercial-Share Alike 3.0
 
-local Measure,Meter,OldValue,StartAngle,RotationTable,rad={},{},{},{},{},math.rad
+local Measure,Meter,OldValue,StartAngle,RotationTable,rad,abs={},{},{},{},{},math.rad,math.abs
 function Initialize()
 	Constant,SustainSensitivity,ChangeSensitivity=SKIN:ReplaceVariables("#Constant#"),SKIN:ReplaceVariables("#SustainSensitivity#"),SKIN:ReplaceVariables("#ChangeSensitivity#")
-	Regression,Direction=SKIN:ParseFormula(SKIN:ReplaceVariables("#Regression#")),SKIN:ReplaceVariables("#Direction#")
+	Direction=SKIN:ReplaceVariables("#Direction#")
 	Sub,Index,Limit=SELF:GetOption("Sub"),SKIN:ParseFormula(SELF:GetOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit"))
 	local MeasureName,MeterName,gsub,random=SKIN:ReplaceVariables("#MeasureName#"),SKIN:ReplaceVariables("#MeterName#"),string.gsub,math.random
 	for i=Index,Limit do OldValue[i],StartAngle[i],Measure[i],Meter[i]=0,0,SKIN:GetMeasure((gsub(MeasureName,Sub,i))),(gsub(MeterName,Sub,i))
@@ -12,15 +12,15 @@ function Initialize()
 
 function Update()
 	for i=Index,Limit do 
-		local Difference=Measure[i]:GetValue()-OldValue[i]
-		OldValue[i]=Measure[i]:GetValue()
+		local Value,Difference=Measure[i]:GetValue(),Measure[i]:GetValue()-OldValue[i]
+		OldValue[i]=Value
 		
-		if Regression==0 then if Difference<0 then Difference=0 end end
-		
+		if Difference<0 then Difference=0 end
+			
 		if RotationTable[i]==0 or Direction=="Clockwise" then
-			StartAngle[i]=(StartAngle[i] + SustainSensitivity*Measure[i]:GetValue() + ChangeSensitivity*Difference + Constant)%rad(360)
+			StartAngle[i]=(StartAngle[i] + Value^((Limit/i)/SustainSensitivity) + Difference^((Limit/i)/ChangeSensitivity) + Constant)%rad(360)
 		elseif RotationTable[i]==1 or Direction=="CounterClockwise" then
-			StartAngle[i]=(StartAngle[i] - SustainSensitivity*Measure[i]:GetValue() - ChangeSensitivity*Difference - Constant)%rad(360) end
+			StartAngle[i]=(StartAngle[i] - Value^((Limit/i)/SustainSensitivity) - Difference^((Limit/i)/ChangeSensitivity) - Constant)%rad(360) end
 		
 		SKIN:Bang("!SetOption",Meter[i],"StartAngle",StartAngle[i])
 	end
