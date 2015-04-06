e98687763569b60de78b9838749995464c30458d
alatsombath <alatsombath@users.noreply.github.com>
2015-04-05 21:01:40 -0700

    1.2 (19 March 2015 rev)
    - Changed "SustainSensitivity" and "ChangeSensitivity" variable names to
    "Sustain" and "Decay"
    - The RotationAngle (minimum/maximum roundline length fill based on
    angle) for each band is now derived from the linear interpolation of
    AngleFillMinLowFreq and AngleFillMinHighFreq / AngleFillMaxLowFreq and
    AngleFillMaxHighFreq variables to increase attack prominence

--- Skins/Eye of the Storm/@Resources/EyeoftheStorm.lua
+++ Skins/Eye of the Storm/@Resources/EyeoftheStorm.lua
@@ -1,13 +1,20 @@
--- EyeoftheStorm v1.1
+-- EyeoftheStorm v1.2
 -- LICENSE: Creative Commons Attribution-Non-Commercial-Share Alike 3.0
 
-local Measure,Meter,OldValue,StartAngle,RotationTable,rad,abs={},{},{},{},{},math.rad,math.abs
+local Measure,Meter,OldValue,StartAngle,RotationTable={},{},{},{},{}
 function Initialize()
-	Constant,SustainSensitivity,ChangeSensitivity=SKIN:ReplaceVariables("#Constant#"),SKIN:ReplaceVariables("#SustainSensitivity#"),SKIN:ReplaceVariables("#ChangeSensitivity#")
-	Direction=SKIN:ReplaceVariables("#Direction#")
 	Sub,Index,Limit=SELF:GetOption("Sub"),SKIN:ParseFormula(SELF:GetOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit"))
+	Direction,ResetAngle=SKIN:ReplaceVariables("#Direction#"),math.rad(360)
+	Constant,Sustain,Decay=SKIN:ReplaceVariables("#Constant#"),SKIN:ReplaceVariables("#Sustain#"),SKIN:ReplaceVariables("#Decay#")
+	AngleFillMinLowFreq,AngleFillMinHighFreq=SKIN:ParseFormula(SKIN:ReplaceVariables("#AngleFillMinLowFreq#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#AngleFillMinHighFreq#"))
+	AngleFillMaxLowFreq,AngleFillMaxHighFreq=SKIN:ParseFormula(SKIN:ReplaceVariables("#AngleFillMaxLowFreq#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#AngleFillMaxHighFreq#"))
+	
 	local MeasureName,MeterName,gsub,random=SKIN:ReplaceVariables("#MeasureName#"),SKIN:ReplaceVariables("#MeterName#"),string.gsub,math.random
-	for i=Index,Limit do OldValue[i],StartAngle[i],Measure[i],Meter[i]=0,0,SKIN:GetMeasure((gsub(MeasureName,Sub,i))),(gsub(MeterName,Sub,i))
+	local AngleFillMinAdd,AngleFillMaxAdd=math.abs(AngleFillMinLowFreq-AngleFillMinHighFreq)*(1/Limit),math.abs(AngleFillMaxLowFreq-AngleFillMaxHighFreq)*(1/Limit)
+	for i=Index,Limit do
+		local MeasureNameSub,AngleFillRatio=(gsub(MeasureName,Sub,i)),(AngleFillMinAdd*i)/(AngleFillMaxAdd*i)
+		OldValue[i],StartAngle[i],Measure[i],Meter[i]=0,0,SKIN:GetMeasure(MeasureNameSub),(gsub(MeterName,Sub,i))
+		SKIN:Bang("!SetOption","MeasureAudioMin"..i,"Formula","("..MeasureNameSub.." < "..AngleFillRatio.." ? "..AngleFillRatio.." : "..MeasureNameSub..")")
 		if Direction=="Random" then RotationTable[i]=random(0,1) end end end
 
 function Update()
@@ -18,9 +25,9 @@ function Update()
 		if Difference<0 then Difference=0 end
 			
 		if RotationTable[i]==0 or Direction=="Clockwise" then
-			StartAngle[i]=(StartAngle[i] + Value^((Limit/i)/SustainSensitivity) + Difference^((Limit/i)/ChangeSensitivity) + Constant)%rad(360)
+			StartAngle[i]=(StartAngle[i] + Value^((Limit/i)/Sustain) + Difference^((Limit/i)/Decay) + Constant)%ResetAngle
 		elseif RotationTable[i]==1 or Direction=="CounterClockwise" then
-			StartAngle[i]=(StartAngle[i] - Value^((Limit/i)/SustainSensitivity) - Difference^((Limit/i)/ChangeSensitivity) - Constant)%rad(360) end
+			StartAngle[i]=(StartAngle[i] - Value^((Limit/i)/Sustain) - Difference^((Limit/i)/Decay) - Constant)%ResetAngle end
 		
 		SKIN:Bang("!SetOption",Meter[i],"StartAngle",StartAngle[i])
 	end
